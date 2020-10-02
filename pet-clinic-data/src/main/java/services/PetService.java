04a29468dcebbd78e7764e747afe40261afb6bb4
N_Ght <nihar.gharat@gmail.com>
2020-10-02 05:04:15 -0700

    Common crudService, closes #16

--- pet-clinic-data/src/main/java/services/PetService.java
+++ pet-clinic-data/src/main/java/services/PetService.java
@@ -4,10 +4,6 @@ import com.nihar.learning.sfgpetclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long> {
 
-    Pet findById(Long id);
-    Pet findByLastName(String lastName);
-    Pet save(Pet pet);
-    Set<Pet> finaAll();
 }
--- pet-clinic-data/src/main/java/services/VetService.java
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -5,10 +5,6 @@ import com.nihar.learning.sfgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet findByLastName(String lastName);
-    Vet save(Vet pet);
-    Set<Vet> finaAll();
+public interface VetService extends CrudService<Vet, Long> {
+    
 }
