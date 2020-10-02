6093dd7b25ed0e2d955549bdf3ccb542dc9ec54f
N_Ght <nihar.gharat@gmail.com>
2020-10-02 06:42:11 -0700

    Woner ctrl added

--- pet-clinic-web/src/main/java/com/nihar/learning/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/nihar/learning/sfgpetclinic/controllers/OwnerController.java
@@ -3,11 +3,13 @@ package com.nihar.learning.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping(value = "/owner")
 @Controller
 public class OwnerController {
 
-    @GetMapping(value = {"/owner", "/owner/index", "/owner/index.html"})
+    @GetMapping(value = {"", "/", "/index", "/index.html"})
     public String indexCtrl(Model model) {
         return "owner/index";
     }
